7cb545623dacfeb1774d5a30dea51e589b18f50f
Aziz Yemloul <azizyemloul@gmail.com>
2016-01-08 21:17:06 +0100

    Use a dictionnary to hold sections infos

--- proclocal.py
+++ proclocal.py
@@ -6,11 +6,15 @@ fo = open("s_p_h_p_BeautifulSoup.html", "r+")
 content = fo.read()
 soup = BeautifulSoup(content, 'html.parser')
 
+dict_sections = {}
 for element in soup.find_all(id="codenum"):
     code = element.parent.parent.contents[1].contents[1].contents[1].contents[0]
     spec = re.sub('"','',element.parent.parent.contents[3].contents[1].contents[1].contents[0])
-    print " ".join(code.split()) + " " + " ".join(spec.split())
-
+    #print " ".join(code.split()) + " " + " ".join(spec.split())
+    code = " ".join(code.split())
+    spec = " ".join(spec.split())
+    dict_sections[code] = spec
+print dict_sections
 
 # codenum
 # soup.find(id="codenum").parent.parent.contents[1].contents[1].contents[1].contents[0]
