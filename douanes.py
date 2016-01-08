de88826296613dd41e44beb5c8423c8a91918078
Aziz Yemloul <azizyemloul@gmail.com>
2016-01-08 11:09:19 +0100

    Ajout d'informations en commentaires

--- douanes.py
+++ douanes.py
@@ -1,12 +1,15 @@
 # -*- coding: utf8 -*-
-import urllib2
-import urllib
+import urllib2 # https://docs.python.org/2/howto/urllib2.html
+import urllib # on en aura besoin quand il faudra envoyer des requêtes en GET
+
+# user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' # au cas où ça s'impose
+# headers = { 'User-Agent' : user_agent }
 
 url = 'http://www.douane.gov.dz/applications/tarif/'
 home_req = urllib2.Request(url)                 # création de l'object requête http sur la page d'accueil
-home_response = urllib2.urlopen(home_req)      # objet réponse
+home_response = urllib2.urlopen(home_req)       # objet réponse
 home_page = home_response.read()                # contenu de la réponse
-print home_page
+print home_page                                 # impression du contenu vers sortie standard
 
 """
 À ce stade on peut voir que les numéro et les intitulés des sections sont identifiés respectivement :
