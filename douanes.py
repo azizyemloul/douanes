39aa772c3ee23501c83e40f4bd78ed451dfd9a01
Aziz Yemloul <azizyemloul@gmail.com>
2016-01-08 10:45:55 +0100

    Requête de niveau 1 vers page d'accueil

--- douanes.py
+++ douanes.py
@@ -3,3 +3,13 @@ import urllib2
 import urllib
 
 url = 'http://www.douane.gov.dz/applications/tarif/'
+home_req = urllib2.Request(url)                 # création de l'object requête http sur la page d'accueil
+home_response = urllib2.urlopen(home_req)      # objet réponse
+home_page = home_response.read()                # contenu de la réponse
+print home_page
+
+"""
+À ce stade on peut voir que les numéro et les intitulés des sections sont identifiés respectivement :
+<div id="codenum">
+<div id="descr">
+"""
