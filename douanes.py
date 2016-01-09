7616c1c5f93c12b18d5e6b02c96334847f49868c
Aziz Yemloul <azizyemloul@gmail.com>
2016-01-09 10:27:04 +0100

    Filter home_page throw BSoup before encode('utf8')

--- douanes.py
+++ douanes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import urllib2 # https://docs.python.org/2/howto/urllib2.html
+from bs4 import BeautifulSoup # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
 url = 'http://www.douane.gov.dz/applications/tarif/'
 home_req = urllib2.Request(url)                 # création de l'object requête http sur la page d'accueil
@@ -12,6 +13,9 @@ home_page = home_response.read()                # contenu de la réponse
 <div id="codenum">
 <div id="descr">
 """
+home_page = BeautifulSoup(home_page, 'html.parser')
+home_page = home_page.prettify()
+
 fo = open("s_p_h_p_BeautifulSoup.html", "wb") # enregistrement du contenu dans le fichier s_p_h_p_BeautifulSoup.html
 fo.write(home_page.encode('utf8'))      # encode('utf8') est ici nécessaire car
 #                                       # le site renvoie une réponse encodée en ISO-8859 et python s'en plaint
