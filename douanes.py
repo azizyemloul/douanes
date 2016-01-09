22422d55120ad0377a20d489d751aed1e855a62f
Aziz Yemloul <azizyemloul@gmail.com>
2016-01-09 14:06:31 +0100

    Extraction des pages chapitre

--- douanes.py
+++ douanes.py
@@ -1,12 +1,12 @@
 # -*- coding: utf8 -*-
-import urllib2 # https://docs.python.org/2/howto/urllib2.html
-from bs4 import BeautifulSoup # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+import urllib2                          # https://docs.python.org/2/howto/urllib2.html
+from bs4 import BeautifulSoup           # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
 url = 'http://www.douane.gov.dz/applications/tarif/'
 home_req = urllib2.Request(url)                 # création de l'object requête http sur la page d'accueil
 home_response = urllib2.urlopen(home_req)       # objet réponse
 home_page = home_response.read()                # contenu de la réponse
-#print home_page                                 # impression du contenu vers sortie standard
+#print home_page                                # impression du contenu vers sortie standard
 
 """
 À ce stade on peut voir que les numéro et les intitulés des sections sont identifiés respectivement :
@@ -15,7 +15,7 @@ home_page = home_response.read()                # contenu de la réponse
 """
 home_page = BeautifulSoup(home_page, 'html.parser')
 home_page = home_page.prettify()
-
-fo = open("s_p_h_p_BeautifulSoup.html", "wb") # enregistrement du contenu dans le fichier s_p_h_p_BeautifulSoup.html
-fo.write(home_page.encode('utf8'))      # encode('utf8') est ici nécessaire car
-#                                       # le site renvoie une réponse encodée en ISO-8859 et python s'en plaint
+fo = open("s_p_h_p_BeautifulSoup.html", "wb")   # enregistrement du contenu dans le fichier s_p_h_p_BeautifulSoup.html
+fo.write(home_page.encode('utf8'))              # encode('utf8') est ici nécessaire car
+#                                               # le site renvoie une réponse encodée en ISO-8859 et python s'en plaint
+#                                               # http://stackoverflow.com/questions/19833440/unicodeencodeerror-ascii-codec-cant-encode-character-u-xe9-in-position-7
--- proclocal.py
+++ proclocal.py
@@ -1,6 +1,8 @@
 # -*- coding: utf8 -*-
-from bs4 import BeautifulSoup # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
-import re
+from bs4 import BeautifulSoup   # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+import re                       # https://docs.python.org/2/library/re.html
+import urllib                   # pour envoyer des requêtes en GET
+import urllib2                  # https://docs.python.org/2/howto/urllib2.html
 
 fo = open("s_p_h_p_BeautifulSoup.html", "r+")
 content = fo.read()
@@ -10,17 +12,38 @@ dict_sections = {}
 for element in soup.find_all(id="codenum"):
     code = element.parent.parent.contents[1].contents[1].contents[1].contents[0]                # on navigue 2 parents plus haut pour accéder aux deux cellules du tableau
     spec = re.sub('"','',element.parent.parent.contents[3].contents[1].contents[1].contents[0]) # la cellule comportant les descriptions est inconsistante
-    #                                                                                           # certaines valeurs comportent des guillements d'autres pas, on les élimine
-    #print " ".join(code.split()) + " " + " ".join(spec.split())
-    code = " ".join(code.split())
+    #                                                                                           # certaines valeurs comportent des guillemets d'autres pas, on élimine les guillemets
+    #print " ".join(code.split()) + " " + " ".join(spec.split())                                # a été utilisé pour section1.txt
+    code = " ".join(code.split())                                                               # http://stackoverflow.com/questions/8270092/python-remove-all-whitespace-in-a-string
     spec = " ".join(spec.split())
     dict_sections[code] = spec
-print dict_sections
 
-# codenum
-# soup.find(id="codenum").parent.parent.contents[1].contents[1].contents[1].contents[0]
+url = 'http://www.douane.gov.dz/applications/tarif/get_content.php'
+for idi,v in dict_sections.items():
+    print idi
+    full_url = url + '?id_chapitre=' +  idi
+    req = urllib2.Request(full_url)
+    opn = urllib2.urlopen(req)
+    rep = opn.read()
+    soup = BeautifulSoup(rep, 'html.parser')
+    for element in soup.find_all(id="codenum"):
+        code = element.contents[0].string                                                               # on navigue 2 parents plus haut pour accéder aux deux cellules du tableau
+        spec = element.findNext('td').contents[0].string                                                # la cellule comportant les descriptions est inconsistante
+        #                                                                                               # certaines valeurs comportent des guillemets d'autres pas, on élimine les guillemets
+        #print " ".join(code.split()) + " " + " ".join(spec.split())                                # a été utilisé pour section1.txt
+        code = " ".join(code.split())                                                               # http://stackoverflow.com/questions/8270092/python-remove-all-whitespace-in-a-string
+        if (spec == None) : spec = ""                                                                   #
+        spec = " ".join(spec.split())
+        print "         " + code + " : " + spec
 
-# desc
-# soup.find(id="codenum").parent.parent.contents[3].contents[1].contents[1].contents[0]
-# for element in soup.find_all(id="descr"):
-#     print element.contents[1].contents[0]
+
+#print dict_sections
+#get_content.php?id_chapitre="+id_chapitre+"&id_section="+id_section+"&id_sous_section="+id_sous_section+"&id_article="+id_article
+# id_chapitre = ""
+# id_section = ""
+# id_sous_section = ""
+# id_article = ""
+
+# url = 'http://www.douane.gov.dz/applications/tarif/get_content.php'
+# full_url = url + '?id_chapitre=' + "02"
+# print resp1
